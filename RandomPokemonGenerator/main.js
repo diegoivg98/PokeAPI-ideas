368a1cf9f5484549e55f36cac12855f5dd0f671a
Diego Vistoso <68931257+diegoivg98@users.noreply.github.com>
2022-07-13 00:28:43 -0400

    Update main.js
    cambio en el img utilizando en el src el siguiente link https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png

--- RandomPokemonGenerator/main.js
+++ RandomPokemonGenerator/main.js
@@ -6,15 +6,14 @@ getPokemon.addEventListener("click", () => {
         .then((response) => response.json())
         .then((response) => {
             displayPokemon(response.name.charAt(0).toUpperCase() + response.name.slice(1),
-                           response.sprites.front_default,
                            response.id);
         });
 });
 
-const displayPokemon = (name,img,id) =>{
+const displayPokemon = (name,id) =>{
     randompokemon.innerHTML = `
     <div class="card card border-info mb-3" style="width: 20rem;">
-        <img src="${img}" class="card-img-top" alt="${name}">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" class="card-img-top" alt="${name}">
             <div class="card-body">
             <span>N.º${id}</span>
                 <h5 class="card-title">${name}</h5>
